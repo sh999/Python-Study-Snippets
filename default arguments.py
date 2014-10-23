ca08054993c418da2b08ad7998694c04013d03ee
sh999 <satrio.h@gmail.com>
2014-10-22 22:29:56 -0400

    added default arguments.py

---
+++ default arguments.py
@@ -0,0 +1,28 @@
+'''
+default arguments.py
+Task:
+Utilize default keywords for a function with an immutable type (number, string, etc.)
+Utilize default keywords with a mutable object (list, dictionary, etc.)
+	For example, define a function with an empty list and 
+	What's the difference?
+Reference:
+https://docs.python.org/2/tutorial/controlflow.html#default-argument-values
+
+'''
+def printInfo(name, species="human"):
+	print "Your name is",name
+	print "You are a",species
+
+def addMember(member, tribe=[]):
+	print "tribe before appending",tribe
+	tribe.append(member)
+	return tribe
+
+printInfo("Bob")
+print addMember("Jack")
+print addMember("Lucy")
+print addMember("Lala") # The default keyword of empty list is executed int he first function call and not latter, so list keeps growing
+
+
+
+
