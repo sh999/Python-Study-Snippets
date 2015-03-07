f3806ce695f3f21eb2d3af5ad8d945a4c5d66e3d
sh999 <satrio.h@gmail.com>
2015-03-07 15:37:25 -0500

    Modified filter.py

--- filter.py
+++ filter.py
@@ -1,7 +1,16 @@
 '''
 filter.py
+Task
+====
 Create a list of numbers
-Create a new list using filter that has elements of above list that is filtered by a function
+Create a new list using filter that has elements of above list 
+	that is filtered by a function
+E.g.
+	Create a list of numbers 1-1000
+	Create a new list by filter that filters above list
+		so that the new list has numbers 900-1000
+
+Lesson status: Complete
 '''
 def checkOver9000(a):
 	if a > 9000:
