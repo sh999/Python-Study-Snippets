de9598e5c71f12dbc657d6a022fb8272ac174ff5
sh999 <satrio.h@gmail.com>
2015-06-15 18:14:05 -0400

    math study added

---
+++ math_study.py
@@ -0,0 +1,20 @@
+'''
+math study
+Use pythons math library
+Compute sine of pi/2 radians
+Compute sine of 90 degrees
+Compute hypotenuse of triangle with sides 3, 4
+Compute log 100
+Compute log 100 base 5
+1) What happens when the log function is used with only one parameter?
+Compute 3^4
+'''
+
+from math import  *
+print cos(pi/2)
+print cos(radians(90))
+print hypot(3,4)
+print log(100, 10)
+print log(100, 5)
+print log(100) # 1) With one parameter, e is the default base
+print pow(3,4)
